4ee1e155e200a513fa074b0a3f6da3c5e734da06
drlocon <104292397+drlocon@users.noreply.github.com>
2022-05-06 09:55:28 +0000

    [Update]エラーメッセージの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,12 +10,10 @@ class BooksController < ApplicationController
       flash[:notice] = 'Book was successfully created.'
       redirect_to book_path(@book.id)
     else
-      flash.now[:alert] = 'Titile or Body can' "'" 't be blank'
+      @books = Book.all
+      flash[:notice] = 'Titile or Body can' "'" 't be blank'
       render :index
     end
-    #book = Book.new(book_params)
-    #book.save
-    #redirect_to book_path(book.id)
   end
 
   def show
@@ -27,9 +25,11 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = 'Book was successfully update.'
+      redirect_to book_path(@book.id)
+    end
   end
 
   def destroy
